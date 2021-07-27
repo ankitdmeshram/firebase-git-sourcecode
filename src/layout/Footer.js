959ad666d9d500565b0bd071ff7f8d45883c9654
Ankit <ankitdm69@gmail.com>
2021-07-27 12:13:18 +0530

    Initial Commit

---
+++ src/layout/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container } from 'reactstrap'
+
+
+const Footer = () => {
+    return (
+        <Container
+            fluid
+            tag="footer"
+            className="text-center bg-info text-white text-uppercase fixed-bottom p-3"
+        >
+            Github Search App With Firebase
+        </Container>
+    )
+}
+
+export default Footer
